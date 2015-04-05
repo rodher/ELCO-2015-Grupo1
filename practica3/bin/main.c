276784f6ad3bd2a852fde7dc98e5925eba56845b
Rodrigo Hernangomez <rodrigoherher@gmail.com>
2015-04-05 23:10:45 +0200

    Añadido timestamp real

--- practica3/bin/main.c
+++ practica3/bin/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #define MAIL_CMD "./scripts/sendmail.sh "
 #define PIC_CMD "./scripts/takepicture.sh "
@@ -7,11 +8,15 @@
 
 int main(int argc, char const *argv[])
 {
-	char timestamp[10];
+
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+
+	char timestamp[30];
 	char mail_command[100];
 	char pic_command[100];
 
-	strcpy(timestamp, " 2015");
+	sprintf(timestamp,"%d-%d-%d_%d_%d_%d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
 	strcpy(pic_command, PIC_CMD);
 	strcat(pic_command, timestamp);
