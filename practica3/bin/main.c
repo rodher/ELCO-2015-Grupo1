675f87caccce875801f7ef76ad751c3c56b51fd1
Rodrigo Hernangomez <rodrigoherher@gmail.com>
2015-04-06 00:48:06 +0200

    Arreglando errores

--- practica3/bin/main.c
+++ practica3/bin/main.c
@@ -16,13 +16,14 @@ int main(int argc, char const *argv[])
 	char mail_command[100];
 	char pic_command[100];
 
-	sprintf(timestamp,"%d-%d-%d_%d_%d_%d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+	sprintf(timestamp,"%d-%d-%d-%d%d%d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
 	strcpy(pic_command, PIC_CMD);
 	strcat(pic_command, timestamp);
 
 	strcpy(mail_command, MAIL_CMD);
 	strcat(mail_command, argv[1]);
+	strcat(mail_command, " ");
 	strcat(mail_command, timestamp);
 
 	system(pic_command);
