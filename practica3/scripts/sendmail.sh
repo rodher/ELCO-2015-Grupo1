2995b8a866510276e1b20ceb20dcdbf869ea8fc6
Rodrigo Hernangomez <rodrigoherher@gmail.com>
2015-04-06 00:54:03 +0200

    Pequeños detalles

--- practica3/scripts/sendmail.sh
+++ practica3/scripts/sendmail.sh
@@ -8,7 +8,7 @@ timestamp=$2
 img_path=${img_dir}/${timestamp}.jpg
 
 body="La puerta de tu casa acaba de ser abierta. Mira quién la ha abierto en el archivo adjunto"
-subject="Puerta abierta"
+subject="Puerta_abierta"
 
 echo $body |  mutt -s $subject $destination -a ${img_path}
 rc=$? 
